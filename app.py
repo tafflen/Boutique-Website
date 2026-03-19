eb8390e1a9331b672b55d7ecb1d8e38f52a8d70f
tafflen <ptafflin@gmail.com>
2026-03-19 21:48:32 +0530

    Added render config files

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import mysql.connector
+import os
 
 db = mysql.connector.connect(
     host="localhost",
@@ -52,4 +53,8 @@ def register():
     return render_template("register.html")
 
 if __name__ == "__main__":  
-    app.run(debug=True)
+#     app.run(debug=True)
+
+# if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
